9dbe44cbb8f7d3d53076f85df8a0c623cf21159b
ohager <oliver@devbutze.com>
2023-06-07 16:38:49 -0300

    fix: seo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,6 @@ import { NavBar } from "@/components/navBar";
 import { Modal } from "@/components/modal";
 import config from "react-reveal/globals";
 import { AppContextProvider } from "@/components/appContext";
-import { MetaTags } from "@/components/metatags";
 
 config({ ssrFadeout: true });
 export default function RootLayout({
@@ -14,13 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="light">
-      <MetaTags
-        title="Nostrum"
-        description="Get your global and decentralized Nostr Identifier (NIP05) for free! The Signum blockchain provides you a decentralized Id (Alias), which you own without the need of a centralized domain. Be discoverable worldwide, independent of Nostr relays. Your keys, Your Id!"
-        keywords="nostr, nip05, signum, web3, blockchain, decentralized technology, decentralized identity"
-        imgUrl="https://nostrum.network/img/nostrum-seo.jpg"
-        viewport="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-      />
       <body className="scrollbar-thin scrollbar-thumb-pink-500 scrollbar-track-neutral scrollbar-thumb-rounded-xl">
         <AppContextProvider>
           <NavBar />
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,10 @@
 
 import { ChooseNameSection } from "../sections/chooseName";
 import { ConnectWalletSection } from "../sections/connectWallet";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { ClaimAliasSection } from "../sections/claimAlias";
 import { WelcomeSection } from "../sections/welcome";
+import { MetaTags } from "@/components/metatags";
 
 export default function Page() {
   const chooseNameSectionRef = useRef<HTMLDivElement>();
@@ -65,6 +66,13 @@ export default function Page() {
 
   return (
     <div>
+      <MetaTags
+        title="Nostrum"
+        description="Get your global and decentralized Nostr Identifier (NIP05) for free! The Signum blockchain provides you a decentralized Id (Alias), which you own without the need of a centralized domain. Be discoverable worldwide, independent of Nostr relays. Your keys, Your Id!"
+        keywords="nostr, nip05, signum, web3, blockchain, decentralized technology, decentralized identity"
+        imgUrl="https://nostrum.network/img/nostrum-seo.jpg"
+        viewport="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      />
       <div
         className="min-h-screen"
         style={{
