ab3e8b2e12a898b6a6dbb1870088c02c1f884803
ohager <oliver@devbutze.com>
2023-06-07 18:49:10 -0300

    fix: uncommented metadata:

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Metadata } from "next";
 import { Home } from "../features/home";
 
-// export const metadata: Metadata = {
-//   title: "Nostrum",
-//   applicationName: "Nostrum",
-//   keywords: ["nostr", "nip05", "signum", "web3", "blockchain", "decentralized technology", "decentralized identity"],
-//   viewport: {
-//     minimumScale:1,
-//     viewportFit: 'cover',
-//     userScalable: false,
-//     initialScale: 1,
-//     width: "device-width",
-//   },
-//   description: "Get your global and decentralized Nostr Identifier (NIP05) for free! The Signum blockchain provides you a decentralized Id (Alias), which you own without the need of a centralized domain. Be discoverable worldwide, independent of Nostr relays. Your keys, Your Id!",
-// }
+export const metadata: Metadata = {
+  title: "Nostrum",
+  applicationName: "Nostrum",
+  keywords: [
+    "nostr",
+    "nip05",
+    "signum",
+    "web3",
+    "blockchain",
+    "decentralized technology",
+    "decentralized identity",
+  ],
+  viewport: {
+    minimumScale: 1,
+    viewportFit: "cover",
+    userScalable: false,
+    initialScale: 1,
+    width: "device-width",
+  },
+  description:
+    "Get your global and decentralized Nostr Identifier (NIP05) for free! The Signum blockchain provides you a decentralized Id (Alias), which you own without the need of a centralized domain. Be discoverable worldwide, independent of Nostr relays. Your keys, Your Id!",
+};
 export default function Page() {
   return (
     <div>
