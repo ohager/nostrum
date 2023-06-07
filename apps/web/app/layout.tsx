b906f1f6c397c1bb802e883858c6d924e88ba57b
ohager <oliver@devbutze.com>
2023-06-07 16:35:01 -0300

    chore: meta description

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
     <html lang="en" data-theme="light">
       <MetaTags
         title="Nostrum"
-        description="Get your global and decentralized Nostr Identifier (NIP05) for free!"
-        keywords="nostr, nip05, signum, web3, blockchain, decentralized technology"
+        description="Get your global and decentralized Nostr Identifier (NIP05) for free! The Signum blockchain provides you a decentralized Id (Alias), which you own without the need of a centralized domain. Be discoverable worldwide, independent of Nostr relays. Your keys, Your Id!"
+        keywords="nostr, nip05, signum, web3, blockchain, decentralized technology, decentralized identity"
         imgUrl="https://nostrum.network/img/nostrum-seo.jpg"
         viewport="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
