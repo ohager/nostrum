993b34682f32bad6fbbe49b261d86b7af6f68b27
ohager <oliver@devbutze.com>
2023-06-07 19:37:27 -0300

    chore/demo: minor improvements

--- apps/search-demo/main.js
+++ apps/search-demo/main.js
@@ -4,28 +4,28 @@ import "nostrum-name-search";
 
 document.querySelector("#app").innerHTML = `
     <div class="watermark"></div>
-  <div class="relative mx-auto max-w-lg mt-[12%] h-fit">
+  <div class="relative mx-auto max-w-2xl w-full mt-[12%] h-fit">
     <div class="fixed right-2 top-2 w-[24px] opacity-80 hover:opacity-90 transition-opacity">
       <a href="https://github.com/ohager/nostrum" target="_blank" rel="noopener noreferrer">
           <img src="/github-logo.svg" alt="Github Logo">
       </a>
     </div>
     <h1 class="text-4xl text-center my-4 w-full">Nostrum Search Demo!</h1>
-    <p class="text-xs text-info opacity-50 py-4">
-    This app demonstrates how to use the Nostr Name Search web component. It fetches a so-called
+    <p class="text-sm text-info opacity-50 py-4">
+    This app demonstrates how to use the <a class="link" target="_blank" rel="noreferrer noopener" href="https://github.com/ohager/nostrum/blob/main/packages/nostrum-name-search/README.md">Nostr Name Search web component.</a> It fetches a so-called
     <em>Alias</em> from the <a class="link" href="https://signum.network" rel="noreferrer noopener" target="_blank">Signum blockchain</a> and shows its content. This makes it possible to
     search for <a class="link" href="https://github.com/nostr-protocol/nips/blob/master/05.md" rel="noreferrer noopener" target="_blank">Nostr NIP05 identifiers</a> without relying on relays, i.e. a global decentralized registry
     for Nostr accounts.
     </p>
     
     <div class="text-center py-4">
-      <a href="https://nostrum.vercel.app" rel="noopener noreferrer" target="_blank">
+      <a href="https://nostrum.network" rel="noopener noreferrer" target="_blank">
           <button class="btn btn-accent">Get Your Name!</button>
       </a>
     </div>
     
     <!-- here we use the web component -->
-    <nostrum-name-search id="name-search" signumtld="nostr"></nostrum-name-search>
+    <nostrum-name-search id="name-search"></nostrum-name-search>
     <div class="relative">
     <div id="output" class="mockup-code text-sm">
         <pre data-prefix="$"><code>type something...</code></pre>
