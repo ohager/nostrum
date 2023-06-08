45e4daef7398f192e0bc22e26bce75e3eefd1b35
ohager <oliver@devbutze.com>
2023-06-08 18:06:47 -0300

    ci: simpler update script

--- update-service.sh
+++ update-service.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-rm -rf yarn.lock
 git pull
-yarn install --prod
 cd apps/service
-yarn build
-echo "To restart service type:"
-echo "pm2 restart pm2.config.js"
+pm2 restart pm2.config.js
